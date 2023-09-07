92a4ebddf5e067e488c5ce3957d428a76b2708a3
MadSword12 <144297433+MadSword12@users.noreply.github.com>
2023-09-06 23:24:36 -0300

    Add files via upload

---
+++ ejercicios/ejercicio1.1.py
@@ -0,0 +1,43 @@
+#C:\Python
+#Fabian Riveros
+def abrir_archivo(nombre):
+    ar=open("SetdePruebas.txt" , "r")
+    linea = ar.readline()
+    ar.close()
+    return ar
+
+def desarrollo(ar):
+    
+
+
+def archivosal(nombre):
+    nombre.open(nombre ,"w")
+    nombre.write(nombre)
+    nombre.write("CASO 3688580-4\n")
+    nombre.write("Cantidad \n", )
+    nombre.write("Unitario \n" , )
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    abrir_archivo("SetdePruebas.txt")
+    archivosal = "caso4.dat"
+    elcaso = "CASO 3688580-4"
